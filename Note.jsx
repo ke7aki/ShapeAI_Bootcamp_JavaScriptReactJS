d58c3f80743b8a277b121e6858739005ae62693d
ke7aki <85433453+ke7aki@users.noreply.github.com>
2021-06-06 14:28:09 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+function Note() {
+  return (
+    <div classname="class1" id="note">
+      <h1>JavaScript and ReactJS</h1>
+       <ul>
+        <li>
+          JavaScript abbreviated as JS, 
+          is a programming language that conforms 
+          to the ECMAScript specification.
+        </li>
+        <li>
+          React is an open-source front-end 
+          JavaScript library for building
+          UI components.
+        </li>
+      </ul> 
+    </div>
+  );
+}
+export default Note;
